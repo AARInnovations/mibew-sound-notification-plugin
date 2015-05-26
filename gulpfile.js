7cd1728cf95b38ed8bd6a07847485b46d041b623
AARInnovations <AARInnovations@users.noreply.github.com>
2015-05-26 20:58:56 +0300

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var bower = require('bower'),
+    eventStream = require('event-stream'),
+    gulp = require('gulp'),
+    chmod = require('gulp-chmod'),
+    zip = require('gulp-zip'),
+    tar = require('gulp-tar'),
+    gzip = require('gulp-gzip'),
+    rename = require('gulp-rename');
+
+// Installs bower dependencies
+gulp.task('bower', function(callback) {
+    bower.commands.install([], {}, {})
+        .on('error', function(error) {
+            callback(error);
+        })
+        .on('end', function() {
+            callback();
+        });
+});
+
+gulp.task('prepare-release', ['bower'], function() {
+    var version = require('./package.json').version;
+
+    return eventStream.merge(
+        getSources()
+            .pipe(zip('sound-notification-plugin-' + version + '.zip')),
+        getSources()
+            .pipe(tar('sound-notification-plugin-' + version + '.tar'))
+            .pipe(gzip())
+    )
+    .pipe(chmod(0644))
+    .pipe(gulp.dest('release'));
+});
+
+// Builds and packs plugins sources
+gulp.task('default', ['prepare-release'], function() {
+    // The "default" task is just an alias for "prepare-release" task.
+});
+
+/**
+ * Returns files stream with the plugin sources.
+ *
+ * @returns {Object} Stream with VinylFS files.
+ */
+var getSources = function() {
+    return gulp.src([
+            'Plugin.php',
+            'README.md',
+            'LICENSE',
+            'js/*',
+            'vendor/ion.sound/ion.sound.js'
+        ],
+        {base: './'}
+    )
+    .pipe(rename(function(path) {
+        path.dirname = 'AARInnovations/Mibew/Plugin/SoundNotification/' + path.dirname;
+    }));
+}
